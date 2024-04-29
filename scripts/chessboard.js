e33b8e3e3eef09e384d7314cea43f8cbd9f9ed65
kararAcode <63989704+kararAcode@users.noreply.github.com>
2024-04-28 19:41:38 -0600

    write setupPieces()

--- scripts/chessboard.js
+++ scripts/chessboard.js
@@ -1,6 +1,7 @@
 class Chessboard {
     constructor() {
         this.board = Array(8).fill(null).map(() => Array(8).fill(null));
+        this.setupPieces();
         
         this.cellWidth = width/8;
         this.cellHeight = height/8;
@@ -13,9 +14,35 @@ class Chessboard {
 
         else {
             this.cellHeight = this.cellWidth;
-        }
+        }   
+
+
+
 
     }
+
+
+    setupPieces() {
+        const mainForces = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook];
+
+
+        //setup black pieces
+        for (let i = 0; i < mainForces.length; i++) {
+            this.board[0][i] = new mainForces[i](0, i, "b", this);
+            this.board[1][i] = new Pawn(1, i, "b", this);
+
+        }
+
+        //setup white pieces
+        for (let i = 0; i < mainForces.length; i++) {
+            this.board[6][i] = new Pawn(6, i, "w", this);
+            this.board[7][i] = new mainForces[i](7, i, "w", this);
+        }
+
+
+    }
+    
+    
 
     display() {
         for (let i = 0; i < 8; i++) {
